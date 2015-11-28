90ffd17b41518f33b33d434f919c95820dc983a1
LeonardoArgolo <leonardo.argolo1906@gmail.com>
2015-11-27 22:24:32 -0200

    Update Caixa.java

--- Caixa.java
+++ Caixa.java
@@ -5,16 +5,14 @@ public class Caixa {
     
     
     public static void main(String[] args){
-        // Declarando as variáveis, Scanner e Random
         String nome;
         double inicial;
         Scanner entrada = new Scanner(System.in);
         Random numero = new Random();
         int conta = 1 + numero.nextInt(9999);
     
-        //Obtendo os dados iniciais do Cliente
         System.out.println("Cadastrando novo cliente.");
-        System.out.print("Ente com seu nome: ");
+        System.out.print("Digite o nome do clientee: ");
         nome = entrada.nextLine();
         
         System.out.print("Entre com o valor inicial depositado na conta: ");
